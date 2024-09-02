1bd89b3601b51419e5a31493370ecce53dfd5875
Kiriil <kirillprofi007@gmail.com>
2024-09-02 20:41:01 +0300

    ft: added GameManagers

---
+++ Assets/Scripts/Game/ClientGameManager.cs
@@ -0,0 +1,40 @@
+﻿using Mirror;
+using System;
+
+namespace Assets.Scripts.Game
+{
+    public enum ClientGameState
+    {
+        Loading, // Состояние означает, что началась загрузка геймплея
+        Gameplay, // Состояние означает, что игрок начал игровой цикл
+    }
+
+    public class ClientGameManager : NetworkBehaviour
+    {
+        private ClientGameState _currentState;
+        public ClientGameState CurrentState
+        {
+            get { return _currentState; }
+            private set
+            {
+                _currentState = value;
+                GameStateChanged?.Invoke(value);
+            }
+        }
+
+        public event Action<ClientGameState> GameStateChanged;
+
+
+        [Client]
+        public void StartLoading()
+        {
+            CurrentState = ClientGameState.Loading;
+        }
+
+        [Client]
+        public void StartGameplay()
+        {
+            CurrentState = ClientGameState.Gameplay;
+        }
+    }
+}
---
+++ Assets/Scripts/Game/ServerGameManager.cs
@@ -0,0 +1,52 @@
+﻿using Mirror;
+using System;
+
+namespace Assets.Scripts.Game
+{
+    public enum ServerGameState : byte
+    {
+        Loading, // Состояние означает, что началась загрузка геймплея
+        Generating, // Состояние означает, что мир начал генерироваться
+        Gameplay, // Состояние означает, что игрок начал игровой цикл
+    }
+
+    public class ServerGameManager : NetworkBehaviour
+    {
+        [SyncVar(hook = nameof(OnGameStateChanged))] private ServerGameState _currentState;
+        public ServerGameState CurrentState
+        {
+            get { return _currentState; }
+            private set
+            {
+                _currentState = value;
+                GameStateChanged?.Invoke(value);
+            }
+        }
+
+        public event Action<ServerGameState> GameStateChanged;
+
+        [Client]
+        private void OnGameStateChanged(ServerGameState old, ServerGameState newState)
+        {
+            GameStateChanged?.Invoke(newState);
+        }
+
+        [Server]
+        public void StartLoading()
+        {
+            CurrentState = ServerGameState.Loading;
+        }
+
+        [Server]
+        public void StartGameplay()
+        {
+            CurrentState = ServerGameState.Gameplay;
+        }
+
+        [Server]
+        public void StartGenerate()
+        {
+            CurrentState = ServerGameState.Generating;
+        }
+    }
+}
