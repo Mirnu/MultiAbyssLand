6dcb3d610497ebf5005a94c023b78c34716ea1d2
Kiriil <kirillprofi007@gmail.com>
2024-09-04 14:48:22 +0300

    ft: added cd-util

---
+++ Assets/Scripts/Misc/CD/CDUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Misc.CD
+{
+    public static class CDUtils
+    {
+        public static Action CycleWait(int time, Action callback)
+        {
+            float start = Time.time;
+
+            return () =>
+            {
+                if (start + time > Time.time) return;
+                start = Time.time;
+                callback();
+            };
+        }
+    }
+}
