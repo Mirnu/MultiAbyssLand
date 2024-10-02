a74c6bde3a8cb0f39d6f514ae1c3b3ec9516aa33
DoOoMB <goworuha.egor@yandex.ru>
2024-10-02 20:10:00 +0300

    Entity spawner

---
+++ Assets/Scripts/Entity/EntitySpawner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEditor;
+using NavMeshPlus.Components;
+
+public class EntitySpawner : NetworkBehaviour
+{
+    public GameObject EntityPrefab;
+    public float spawnCooldown = 1f;
+    public bool isSpawning = true;
+    public float radius = 3f;
+    private float _curTime = 0f;
+
+    [Server]
+    public void Update()
+    {
+        if (isSpawning)
+        {
+            if (Time.time - _curTime >= spawnCooldown)
+            {
+                Vector2 pos = (Vector2)transform.position + Random.insideUnitCircle * radius;
+                var zombie = Instantiate(EntityPrefab, pos, new Quaternion());
+                NetworkServer.Spawn(zombie);
+                _curTime = Time.time;
+            }
+            
+        }
+    }
+
+}
