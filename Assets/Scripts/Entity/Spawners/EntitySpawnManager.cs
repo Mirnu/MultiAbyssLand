2f2a0b636f16662ebeeb24e4b0cf0bcca9188d33
DoOoMB <goworuha.egor@yandex.ru>
2024-10-10 18:16:03 +0300

    entity spawn

--- Assets/Scripts/Entity/Spawners/EntitySpawnManager.cs
+++ Assets/Scripts/Entity/Spawners/EntitySpawnManager.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
-
+using System.Linq;
+using System;
+using Random = UnityEngine.Random;
 
 namespace Assets.Scripts.Entity
 {
@@ -12,9 +14,13 @@ namespace Assets.Scripts.Entity
         [SerializeField] private float _spawnRateMin = 10f;
         [SerializeField] private float _playerSimDistance = 30f;
         [SerializeField] private float _playerViewDistance = 10f;
+        [SerializeField] private int _maxEntitiesSimCount = 30;
 
         private float spawnCircleTime = 0f;
         private float curSpawnRate = 0f;
+
+        private List<GameObject> _mobPool = new List<GameObject>();
+        private PlayerFacade[] players;
 
         [HideInInspector] public EntitySpawnManager Instance;
 
@@ -24,14 +30,23 @@ namespace Assets.Scripts.Entity
             else Destroy(this);
         }
 
+        private void Start()
+        {
+            
+        }
+
         private void Update()
         {
+            players = (PlayerFacade[])FindObjectsByType(typeof(PlayerFacade), FindObjectsSortMode.None);
             if (Time.time - spawnCircleTime > curSpawnRate)
             {
-
+                SpawnRandomly();
+                curSpawnRate = Random.Range(_spawnRateMin, _spawnRateMax);
+                spawnCircleTime = Time.time;
             }
         }
 
+        [Server]
         private void SpawnRandomly()
         {
             foreach (PlayerFacade player in FindObjectsByType(typeof(PlayerFacade), FindObjectsSortMode.None))
@@ -39,13 +54,36 @@ namespace Assets.Scripts.Entity
                 Vector3 playerPos = player.gameObject.transform.position;
                 foreach (SpawnData mob in _mobSpawnData)
                 {
+                    if (_mobPool.Count >= _maxEntitiesSimCount) continue;
                     float rate = Random.Range(1, 101);
                     if (rate < mob.SpawnChance * 100) continue;
-                    /*float x_min = Random.Range(playerPos.x - _playerViewDistance, playerPos.x + _playerViewDistance);
-                    float y = Random.Range(playerPos.y - _playerViewDistance, playerPos.y + _playerViewDistance);*/
-                    // логика спавна и проч.
+                    float x_min = Random.Range(Math.Min(playerPos.x - _playerViewDistance, playerPos.x - _playerSimDistance), Math.Max(playerPos.x - _playerViewDistance, playerPos.x - _playerSimDistance));
+                    float x_max = Random.Range(Math.Min(playerPos.x + _playerSimDistance, playerPos.x + _playerViewDistance), Math.Max(playerPos.x + _playerViewDistance, playerPos.x + _playerSimDistance));        
+                    float y_min = Random.Range(Math.Min(playerPos.y - _playerSimDistance, playerPos.y - _playerViewDistance), Math.Max(playerPos.y - _playerSimDistance, playerPos.y - _playerViewDistance));
+                    float y_max = Random.Range(Math.Min(playerPos.y + _playerSimDistance, playerPos.y + _playerViewDistance), Math.Min(playerPos.y + _playerSimDistance, playerPos.y + _playerViewDistance));
+                    float x = Random.Range(0f, 1f) < 0.5 ? x_min : x_max;
+                    float y = Random.Range(0f, 1f) < 0.5 ? y_min : y_max;
+                    var entity = Instantiate(mob.Prefab, new Vector3(x, y, 0), new Quaternion());
+                    NetworkServer.Spawn(entity);
                 }
             }
+        }
+
+        private void OnDrawGizmos()
+        {
+            
+            foreach (PlayerFacade player in players)
+            {
+                Debug.Log("DRAWWWW");
+                Gizmos.color = Color.green;
+                Gizmos.DrawWireCube(player.gameObject.transform.position, new Vector3(_playerSimDistance*2, _playerSimDistance*2, 1));
+                Gizmos.DrawWireCube(player.gameObject.transform.position, new Vector3(_playerViewDistance*2, _playerViewDistance*2, 1));
+            }
+        }
+
+        private void UpdatePool()
+        {
+            _mobPool = (List<GameObject>)_mobPool.Where(item => item != null);
         }
     }
 }
