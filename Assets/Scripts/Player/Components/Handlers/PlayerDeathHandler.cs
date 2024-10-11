19df9538af1dee99826dbbd8f2ac849121762561
Kiriil <kirillprofi007@gmail.com>
2024-10-11 11:03:03 +0300

    ft: added playerDeathHandler

--- Assets/Scripts/Player/Components/Handlers/PlayerDeathHandler.cs
+++ Assets/Scripts/Player/Components/Handlers/PlayerDeathHandler.cs
@@ -1,5 +1,7 @@
 ﻿using Assets.Scripts.Player.Data;
+using Mirror;
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Assets.Scripts.Player.Components.Handlers
@@ -7,18 +9,35 @@ namespace Assets.Scripts.Player.Components.Handlers
     public class PlayerDeathHandler : PlayerComponent
     {
         [SerializeField] private PlayerStats _stats;
+        [SerializeField] private GameObject _character;
+        [SerializeField] private GameObject _controllers;
 
         public override void ServerInitialize()
         {
             _stats.HealthChanged += OnHealthChanged;
         }
 
+        [Server]
         private void OnHealthChanged(int health)
         {
             if (health == 0)
             {
-
+                Camera.main.transform.SetParent(playerManager.transform);
+                _character.SetActive(false);
+                _controllers.SetActive(false);
+                StartCoroutine(Spawn());
             }
+        }
+
+        private IEnumerator Spawn()
+        {
+            yield return new WaitForSeconds(5);
+            Camera.main.transform.SetParent(_character.transform);
+            _character.SetActive(true);
+            _controllers.SetActive(true);
+            _character.transform.position = new Vector3(0, 0, 6.078432f);
+            _stats.Health = 50;
+            _stats.Mana = 50;
         }
     }
 }
