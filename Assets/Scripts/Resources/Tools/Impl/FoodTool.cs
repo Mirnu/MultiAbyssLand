cbfd1788b56f1ee46165ffbcfa7762f7663551d2
Kiriil <kirillprofi007@gmail.com>
2024-10-13 15:04:10 +0300

    ft: tool

---
+++ Assets/Scripts/Resources/Tools/Impl/FoodTool.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Scripts.Player.Inventory.Hotbar;
+using Assets.Scripts.Resources.Data;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.Scripts.Resources.Tools.Impl
+{
+    public class FoodTool : ToolBehaviour
+    {
+        private PlayerFacade _facade => PlayerFacade.Singleton;
+        private ContainerHotbarSlots _hotbarSlots => _facade.hotbar;
+
+        protected override void OnActivated(InputAction.CallbackContext context)
+        {
+            _facade.Stats.Food += tool.GetResource<FoodResource>().Satiety;
+            _hotbarSlots.DeleteFromSlot();
+        }
+    }
+}
