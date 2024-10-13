67293fdacb4885ddf246fd1b7af868eca7306a2e
Kiriil <kirillprofi007@gmail.com>
2024-10-13 17:58:12 +0300

    fix bugs

---
+++ Assets/Scripts/Misc/MainMenuSoundController.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Scripts.Misc.Managers;
+using UnityEngine;
+
+namespace Assets.Scripts.Misc
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class MainMenuSoundController : MonoBehaviour
+    {
+        private AudioSource _audioSource;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        private void Update()
+        {
+            _audioSource.volume = SoundSettings.MusicVolume * SoundSettings.MasterVolume;
+        }
+    }
+}
